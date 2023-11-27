5c95f47488e51ee76b5fb6870dffa89c5e2684b2
abhi3700 <dalvath3700@gmail.com>
2023-11-28 02:29:27 +0530

    Added random identity generation feature

--- sdk-rs/examples/create_id_random.rs
+++ sdk-rs/examples/create_id_random.rs
@@ -4,12 +4,28 @@
 //!
 //! Commitment is to be used anywhere you want in public
 //! like adding into a MT.
+//!
+//! PR submitted: https://github.com/worldcoin/semaphore-rs/pull/59
+//!
+//! ## Use case
+//!
+//! - Developing a method to assign unique IDs (commitments)
+//! to users and securely manage their identity
+//! secrets (trapdoor & nullifier) in a custodial setup.
 
+use rand::Rng;
 use semaphore::identity::Identity;
 
 fn main() {
-    // TODO: Create a function `Identity::random()` with random secret as PR.
-    let mut secret = *b"";
+    let mut rng = rand::thread_rng();
+    let random_number: u32 = rng.gen();
+
+    // num to string to bytes
+    let random_string = random_number.to_string();
+    let secret_bytes = random_string.as_bytes();
+
+    // convert bytes to a fixed-length byte array, you'll need to adjust this.
+    let mut secret = Vec::from(secret_bytes);
     let id = Identity::from_secret(&mut secret, None);
     println!("Id secret: {}", id.secret_hash());
     println!("Trapdoor (secret): {}", id.trapdoor);
